728c6b97b12d8549aead557e7bcc7840172cd7e9
Yograj47 <yograjrijal926@gmail.com>
2025-03-18 22:08:20 +0545

    Updated and change content and used react

---
+++ src/components/Contact.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone, faLocationDot} from "@fortawesome/free-solid-svg-icons"
+
+function Contact(){
+    return(
+        // <!-- Contact Section -->
+        <div id="contact">
+            <div className="container">
+                <div className="contact-wrapper">
+                    {/* <!-- Contact Info --> */}
+                    <div className="contact-info">
+                        <h2>Get in Touch</h2>
+                        <p><FontAwesomeIcon icon={faEnvelope} / >yograjrijal926@email.com</p>
+                        <p><FontAwesomeIcon icon={faPhone} / >+977-9860256338</p>
+                        <p><FontAwesomeIcon icon={faLocationDot} / >Kathmandu, Nepal</p>
+
+                        {/* <!-- Social Links --> */}
+                        <div className="social-links">
+                            <a href="#" target="_blank"><FontAwesomeIcon icon={faGithub}/></a>
+                            <a href="#" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a>
+                            <a href="#" target="_blank"><FontAwesomeIcon icon={faTwitter}/></a>
+                        </div>
+                    </div>
+
+                    {/* <!-- Contact Form --> */}
+                    <div className="contact-form">
+                        <form>
+                            <input type="text" placeholder="Your Name" required />
+                            <input type="email" placeholder="Your Email" required />
+                            <input type="text" placeholder="Subject" required />
+                            <textarea placeholder="Message" rows="5" required></textarea>
+                            <button type="submit">Send Message</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Contact;
