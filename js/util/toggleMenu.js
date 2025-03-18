3cf673ffc1a6a3d1dafc640243e096ea276ca806
Yograj47 <yograjrijal926@gmail.com>
2025-03-18 22:05:22 +0545

    Delete js directory

--- js/util/toggleMenu.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    document.querySelector(".navBar").classList.toggle("show");
-}
-document.querySelector(".menu-btn").addEventListener("click", toggleMenu);
-document.querySelectorAll(".navBar li").forEach(item => {
-    item.addEventListener("click", toggleMenu);
-});
